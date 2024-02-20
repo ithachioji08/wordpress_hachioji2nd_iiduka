2fa60e3850fba54a4c05e9d1d06a90c2538fbb58
ithachioji08 <146053692+ithachioji08@users.noreply.github.com>
2024-02-20 14:15:37 +0900

    飯塚隼平
    リンクがトップに行くようで微妙に違うのでページネーションがないページにいく不具合を修正、functions,phpのいらない処理を削除

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer>
         <ul>
-            <li class="home"><a href="<?php echo home_url(); ?>/index.html">TOP</a></li>
-            <li><a href="<?php echo home_url(); ?>/service.html">SERVICE</a></li>
-            <li><a href="<?php echo home_url(); ?>/company.html">COMPANY</a></li>
-            <li><a href="<?php echo home_url(); ?>/contact.html">CONTACT</a></li>
+            <li class="home"><a href="<?php echo home_url(); ?>">TOP</a></li>
+            <li><a href="<?php echo home_url(); ?>">SERVICE</a></li>
+            <li><a href="<?php echo home_url(); ?>">COMPANY</a></li>
+            <li><a href="<?php echo home_url(); ?>">CONTACT</a></li>
         </ul>
         <div id="footer">
             <div class="copyright">Copyright &copy; 2020 SAMPLE SITE All Rights Reserved.</div>
--- functions.php
+++ functions.php
@@ -8,11 +8,5 @@ register_nav_menus(
 	'place_footer' => 'フッター',
 	)
 );
-add_theme_support('post-thumbnails');
-add_image_size('thumbside', 85, 85, true);
-add_action( 'parse_query', 'my_parse_query' );
-function my_parse_query( $query ) {
-  if ( ! isset( $query->query_vars['paged'] ) && isset( $query->query_vars['page'] ) )
-    $query->query_vars['paged'] = $query->query_vars['page'];
-}
+
 ?>
