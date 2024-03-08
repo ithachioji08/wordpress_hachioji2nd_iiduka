016acfe314f72e0f85ac55a282d290c8941b7548
ithachioji08 <ithachioji2.520@gmail.com>
2024-03-08 13:37:43 +0900

    飯塚隼平
    フッターのリンクを適切に設定

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer>
         <ul>
             <li class="home"><a href="<?php echo home_url(); ?>">TOP</a></li>
-            <li><a href="<?php echo home_url(); ?>">SERVICE</a></li>
-            <li><a href="<?php echo home_url(); ?>">COMPANY</a></li>
-            <li><a href="<?php echo home_url(); ?>">CONTACT</a></li>
+            <li><a href="<?php echo home_url(); ?>/service">SERVICE</a></li>
+            <li><a href="<?php echo home_url(); ?>/company">COMPANY</a></li>
+            <li><a href="<?php echo home_url(); ?>/contact">CONTACT</a></li>
         </ul>
         <div id="footer">
             <div class="copyright">Copyright &copy; 2020 SAMPLE SITE All Rights Reserved.</div>
